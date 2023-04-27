61afacac726513747d5bc60b166c7ac8acc26f64
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-04-27 09:36:26 -0300

    Create Menu.java

---
+++ Projeto Pousada/Menu.java
@@ -0,0 +1,26 @@
+public class Menu {
+    public void linha(int quantidade){
+        int i;
+        for(i = 0; i < quantidade; i++){
+            System.out.print("-");
+        }
+        System.out.print("\n");
+    }
+    public void espaco(int quantidade){
+        int i;
+        for(i = 0; i < quantidade; i++){
+            System.out.print(" ");
+        }
+    }
+    public void cabecarioPousada(String nome){
+        this.linha(40);
+        this.espaco(14);
+        System.out.print(nome);
+        this.espaco(14);
+        System.out.print("\n");
+        this.linha(40);
+    }
+    public void login(){
+        
+    }
+}
