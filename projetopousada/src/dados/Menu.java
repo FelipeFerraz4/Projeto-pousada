fb2e0d2529904945d806d0dbe3352b22043bd7da
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-04-28 10:46:55 -0300

    Estruturando a classe Menu
    Mudança de nome do método login -> para menuInicial
    Finalização da estrutura do método menuInicial
    Criação e finalização da estrutura do método menuCliente

--- projetopousada/src/dados/Menu.java
+++ projetopousada/src/dados/Menu.java
@@ -24,7 +24,7 @@ public class Menu {
         System.out.print("\n");
         this.linha(40);
     }
-    public void login(){
+    public void menuInicial(){
     	String nome = "Sol Nascente";
     	this.cabecarioPousada(nome);
     	
@@ -44,5 +44,69 @@ public class Menu {
         	}
         }while(escolha>3 || escolha<1);
         
+        if(escolha == 1){
+            System.out.println("Login feito");
+            this.menuCliente();
+            this.menuInicial();
+        }
+        else if(escolha == 2){
+            System.out.println("Cadastrar usuario feito");
+            this.menuInicial();
+        }
+        else{
+            System.out.println("Programa finalizado");
+        }
+    }
+    
+    public void menuGerente(){
+        
+    }
+    public void menuCliente(){
+        String nome = "Sol Nascente";
+    	this.cabecarioPousada(nome);
+    	
+    	int escolha;
+    	
+    	Scanner scan = new Scanner(System.in);
+        System.out.println("1 - Check-in");
+        System.out.println("2 - Sevirco de quarto");
+        System.out.println("3 - Ver consumo");
+        System.out.println("4 - Ver historico de agendamento");
+        System.out.println("5 - Pagar consumo");
+        System.out.println("6 - Checkout");
+        
+        do{
+        	System.out.println("Digite a sua alternativa: ");
+        	escolha = scan.nextInt();
+        	if(escolha>6 || escolha<1) {
+        		System.out.println("Alternativa invalida");
+        	}
+        }while(escolha>6 || escolha<1);
+        
+        switch(escolha){
+            case 1:
+                System.out.println("Check-in feito");
+                this.menuCliente();
+                break;
+            case 2:
+                System.out.println("Sevirco de quarto feito");
+                this.menuCliente();
+                break;
+            case 3:
+                System.out.println("Consumo mostrado");
+                this.menuCliente();
+                break;
+            case 4:
+                System.out.println("historico mostrado");
+                this.menuCliente();
+                break;
+            case 5:
+                System.out.println("Consumo pago");
+                this.menuCliente();
+                break;
+            default:
+                System.out.println("Checkout feito com sucesso");
+                break;
+        }
     }
 }
